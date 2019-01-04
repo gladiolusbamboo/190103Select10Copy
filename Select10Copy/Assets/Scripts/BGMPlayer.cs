3ab8c71b49bec0e959a999507a2ee642e0e493cf
Yuji Koyama <gladiolus_vstl@gladiolus.egoism.jp>
2019-01-04 15:17:49 +0900

    191004Select10Copy

---
+++ Select10Copy/Assets/Scripts/BGMPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMPlayer : MonoBehaviour {
+    public AudioSource[] audioSources;
+
+	// Use this for initialization
+	void Start () {
+        audioSources[0].Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
