449d1fa802134bbc2ea2374206d5b004d7829cd0
Yuji Koyama <gladiolus_vstl@gladiolus.egoism.jp>
2019-01-05 15:20:10 +0900

    190105 Select10Copy

---
+++ Select10Copy/Assets/Scripts/TitleDisplayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+using System;
+
+public class TitleDisplayer : MonoBehaviour
+{
+    public CanvasGroup titleLogoCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 表示が気持ち悪かったので0.1s待つ
+        StartCoroutine(DelayMethod(0.1f, () =>
+        {
+            titleLogoCanvas.DOFade(0.7f, 1.0f).SetEase(Ease.OutSine);
+        }));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator DelayMethod(float waitTime, Action action)
+    {
+        yield return new WaitForSeconds(waitTime);
+        action();
+    }
+}
